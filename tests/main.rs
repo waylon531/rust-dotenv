87d57d082912d5c7913a9a5c924762ce8b078307
Mike Piccolo <mfpiccolo@gmail.com>
2015-12-06 16:27:58 -0800

    Add before for integration tests

--- tests/main.rs
+++ tests/main.rs
@@ -5,11 +5,15 @@ use std::env;
 use std::path::Path;
 use std::fs;
 
+fn before () {
+    let manifest_dir = env!("CARGO_MANIFEST_DIR").to_string() + "/tests";
+    let new_path = Path::new(&manifest_dir);
+    assert!(env::set_current_dir(&new_path).is_ok());
+}
+
 #[test]
 fn test_dotenv() {
-    let new_path = Path::new("./tests");
-    assert!(env::set_current_dir(&new_path).is_ok());
-
+    before();
     dotenv().ok();
     assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
 }
@@ -17,21 +21,22 @@ fn test_dotenv() {
 
 #[test]
 fn make_test_fail() {
+    before();
     let new_path = Path::new("./child");
     assert!(env::set_current_dir(&new_path).is_ok());
     let new_path2 = Path::new("..");
     assert!(env::set_current_dir(&new_path2).is_ok());
 }
 
-/*
-#[test]
-fn test_dotenv_child_dir() {
-    let new_path = Path::new("./src");
-    assert!(env::set_current_dir(&new_path).is_ok());
-    let mut paths = fs::read_dir("./").unwrap();
-    let path_string = paths.nth(0).unwrap().unwrap().path().display().to_string();
-    assert_eq!("./main.rs".to_string(), path_string);
-    dotenv().ok();
-    assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
-}
-*/
+
+// #[test]
+// fn test_dotenv_child_dir() {
+//     let new_path = Path::new("./child");
+//     assert!(env::set_current_dir(&new_path).is_ok());
+//     let mut paths = fs::read_dir("./").unwrap();
+//     let path_string = paths.nth(0).unwrap().unwrap().path().display().to_string();
+//     assert_eq!("./main.rs".to_string(), path_string);
+//     dotenv().ok();
+//     assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
+// }
+
