19ebb1888fe282c9940c5943c9e376c6de230f1c
Alice Maz <alice@alicemaz.com>
2016-01-25 23:50:44 -0800

    More threading fun...

--- tests/main.rs
+++ tests/main.rs
@@ -5,38 +5,40 @@ use std::env;
 use std::path::Path;
 use std::fs;
 
-fn before () {
+fn init() {
     let manifest_dir = env!("CARGO_MANIFEST_DIR").to_string() + "/tests";
     let new_path = Path::new(&manifest_dir);
     assert!(env::set_current_dir(&new_path).is_ok());
 }
 
+fn clear() {
+    env::set_var("TESTKEY", "");
+}
+
 #[test]
 fn test_dotenv() {
-    before();
+    init();
     dotenv().ok();
     assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
+    clear();
 }
-
 
 #[test]
-fn make_test_fail() {
-    before();
+fn test_dotenv_child_dir() {
+    init();
     let new_path = Path::new("./child");
     assert!(env::set_current_dir(&new_path).is_ok());
-    let new_path2 = Path::new("..");
-    assert!(env::set_current_dir(&new_path2).is_ok());
+    dotenv().ok();
+    assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
+    clear();
 }
 
-
-// #[test]
-// fn test_dotenv_child_dir() {
-//     let new_path = Path::new("./child");
-//     assert!(env::set_current_dir(&new_path).is_ok());
-//     let mut paths = fs::read_dir("./").unwrap();
-//     let path_string = paths.nth(0).unwrap().unwrap().path().display().to_string();
-//     assert_eq!("./main.rs".to_string(), path_string);
-//     dotenv().ok();
-//     assert_eq!(env::var("TESTKEY").unwrap(), "test_val");
-// }
-
+#[test]
+fn test_from_filename() {
+    init();
+    from_filename("notenv").ok();
+    assert_eq!(env::var("NOTTESTKEY").unwrap(), "not_test_val");
+    //assert!(!env::var("TESTKEY").is_ok());
+    println!("ff {:?}", env::var("TESTKEY"));
+    clear();
+}
